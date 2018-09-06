ce9681af88217c83f94569f6d683798860d9a38d
Sonnerz <Sonnerz@users.noreply.github.com>
2018-09-06 10:46:30 +0100

    initial commit

---
+++ app.py
@@ -0,0 +1,54 @@
+import os
+from flask import Flask, render_template, redirect, request, url_for
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId
+
+app = Flask(__name__)
+
+app.config["MONGO_DBNAME"] = "task_manager"
+app.config["MONGO_URI"] = "mongodb://admin:topstar1@ds245132.mlab.com:45132/task_manager"
+
+mongo = PyMongo(app)
+
+
+@app.route('/')
+@app.route('/get_tasks') #connection to DB. Function with decorator. Includes route to function. remember the routing is a string that when attached to a URL will redirect to a particular function in a flask application. 
+def get_tasks():
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+
+@app.route('/add_task')
+def add_task():
+    return render_template("addtask.html", categories=mongo.db.categories.find())
+
+@app.route('/insert_task', methods=['POST'])
+def insert_task():
+    tasks = mongo.db.tasks
+    tasks.insert_one(request.form.to_dict()) #convert to dict so that mongo understands the data. Validation needed in HTML and app.py
+    return redirect(url_for('get_tasks'))
+ 
+    
+@app.route('/edit_task/<task_id>')
+def edit_task(task_id):
+    the_task = mongo.db.tasks.find_one({"_id": ObjectId(task_id)})
+    all_categories = mongo.db.categories.find()
+    return render_template("edittask.html", task=the_task, categories=all_categories)
+    
+    
+@app.route('/update_task/<task_id>', methods=['POST'])
+def update_task(task_id):
+    tasks = mongo.db.tasks
+    tasks.update( {'_id': ObjectId(task_id)},
+    { 
+        'task_name' : request.form.get('task_name'),
+        'category_name' : request.form.get('category_name'),
+        'task_description' : request.form.get('task_description'),
+        'due_date' : request.form.get('due_date'),
+        'is_urgent' : request.form.get("is_urgent")
+        
+    })
+    return redirect(url_for('get_tasks'))   
+ 
+    
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'), port=int(os.environ.get('PORT')), debug=True)
